609c960d60bb83386d6c33c3d4bfa2fd4bb81eba
Janno Põldma <janno.poldma@gmail.com>
2013-12-28 11:04:33 +0200

    Initial Nancy self-hosting application template.

---
+++ Jnx/AssemblyInfo.fs
@@ -0,0 +1,15 @@
+module Jnx.AssemblyInfo
+
+open System.Reflection
+open System.Runtime.CompilerServices
+
+[<assembly: AssemblyTitle("Jnx")>]
+[<assembly: AssemblyDescription("Home site")>]
+[<assembly: AssemblyConfiguration("")>]
+[<assembly: AssemblyCompany("")>]
+[<assembly: AssemblyProduct("")>]
+[<assembly: AssemblyCopyright("Janno Põldma")>]
+[<assembly: AssemblyTrademark("")>]
+[<assembly: AssemblyVersion("1.0.0.0")>]
+
+()
---
+++ Jnx/Program.fs
@@ -0,0 +1,51 @@
+module Jnx.Main
+
+open Nancy.Hosting.Self
+open System
+open System.Threading
+
+type RunningMode = Daemon | Application
+
+type CommandLineOptions = {
+    RunningMode : RunningMode
+    Port : int
+    }
+
+let parseCommandLine args =
+    let defaultOptions = { RunningMode = Application; Port = 1234 }
+    let rec parseCommandLineRec args options =
+        match args with
+        | [] -> options
+        | "/D"::xs ->
+            let newOptions = { options with RunningMode = Daemon }
+            parseCommandLineRec xs newOptions
+        | "/P"::xs ->
+            match xs with
+            | [] ->
+                eprintfn "Port number not set."
+                options
+            | p::xss ->
+                let port = int p
+                let newOptions = { options with Port = port }
+                parseCommandLineRec xss newOptions
+        | x::xs ->
+            eprintfn "Option '%s' is unrecognized" x
+            parseCommandLineRec xs options
+    parseCommandLineRec (args |> Seq.toList) defaultOptions
+
+[<EntryPoint>]
+let main args =
+    let options = parseCommandLine args
+
+    let uriString = sprintf "http://localhost:%d" options.Port
+    printfn "Starting self hosting on %s" uriString
+
+    use host = new NancyHost(new Uri(uriString))
+    host.Start()
+
+    match options.RunningMode with
+    | Daemon -> Thread.Sleep(Timeout.Infinite)
+    | _ -> Console.ReadLine() |> ignore
+
+    host.Stop()
+    0
