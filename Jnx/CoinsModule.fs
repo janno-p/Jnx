836bed46b351b8cd363633a3253db7b335f42441
Janno Põldma <janno.poldma@gmail.com>
2013-12-30 16:34:03 +0200

    Trying out models.

--- Jnx/CoinsModule.fs
+++ Jnx/CoinsModule.fs
@@ -3,22 +3,29 @@ namespace Jnx.Modules
 open Jnx.Modules.Utils
 open Nancy
 
+type CoinDetails = {
+    CommemorativeYears : string []
+    }
+
 type CoinsModule() as this =
     inherit NancyModule()
 
+    let details () =
+        { CommemorativeYears = [2004 .. 2014] |> Seq.map (fun x -> x.ToString()) |> Seq.toArray }
+
     do this.Get.["/coins"] <- (fun _ ->
         this.ViewBag?Title <- "Alejandro"
-        this.View.["Index"] :> obj
+        this.View.["Index", details()] :> obj
     )
 
-    do this.Get.["/coins/country/(?<code>^[a-z]{2}$)"] <- (fun args ->
-        sprintf "Country Code: %O" args?code :> obj
+    do this.Get.["/coins/(?<countryCode>^[a-z]{2}$)"] <- (fun args ->
+        sprintf "Country Code: %O" args?countryCode :> obj
     )
 
-    do this.Get.["/coins/year/(?<year>^\d{4}$)"] <- (fun args ->
+    do this.Get.["/coins/(?<year>^\d{4}$)"] <- (fun args ->
         sprintf "Year of %O" args?year :> obj
     )
 
-    do this.Get.["/coins/nominal/(?<value>(^[12]\.00$)|(^0\.[125]0$)|(^0\.0[125]$))"] <- (fun args ->
-        sprintf "Nominal values of %O" args?value :> obj
+    do this.Get.["/coins/(?<nominalValue>(^[12]\.00$)|(^0\.[125]0$)|(^0\.0[125]$))"] <- (fun args ->
+        sprintf "Nominal values of %O" args?nominalValue :> obj
     )
