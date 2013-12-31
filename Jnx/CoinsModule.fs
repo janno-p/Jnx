484833aed8a43f64bca53a10318e4138194cfbb2
Janno Põldma <janno.poldma@gmail.com>
2013-12-31 11:09:20 +0200

    Some layout changes and quick access links.

--- Jnx/CoinsModule.fs
+++ Jnx/CoinsModule.fs
@@ -1,17 +1,25 @@
 namespace Jnx.Modules
 
+open Jnx.Database
 open Jnx.Modules.Utils
 open Nancy
 
-type CoinDetails = {
-    CommemorativeYears : string []
-    }
+type CoinDetails = { CommemorativeYears : string []
+                     Countries : Country []
+                     NominalValues : string [] }
 
 type CoinsModule() as this =
     inherit NancyModule()
 
-    let details () =
-        { CommemorativeYears = [2004 .. 2014] |> Seq.map (fun x -> x.ToString()) |> Seq.toArray }
+    let commemorativeYears = [2004 .. 2014] |> Seq.map (fun x -> x.ToString()) |> Seq.toArray
+    let countries = [|
+        { Id = 1; Code = "et"; Name = "Eesti"; Genitive = "Eesti" }
+        { Id = 2; Code = "at"; Name = "Austria"; Genitive = "Austria" }
+        { Id = 3; Code = "lv"; Name = "Läti"; Genitive = "Läti" }
+    |]
+    let nominalValues = [| "2.00"; "1.00"; "0.50"; "0.20"; "0.10"; "0.05"; "0.02"; "0.01" |]
+
+    let details () = { CommemorativeYears = commemorativeYears; Countries = countries; NominalValues = nominalValues }
 
     do this.Get.["/coins"] <- (fun _ ->
         this.ViewBag?Title <- "Alejandro"
