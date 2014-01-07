bd45f9631ee1e8c9dc443be23b747db758b8acf3
Janno Põldma <janno.poldma@gmail.com>
2014-01-07 17:01:21 +0200

    Code clean up.

--- Jnx.DbMigration/Runner.fs
+++ Jnx.DbMigration/Runner.fs
@@ -3,28 +3,23 @@ module Jnx.DbMigration.Runner
 open Npgsql
 open System.Configuration
 
-let connectionStringName = "Jnx"
-
-let unless condition = not condition
-
-let ConnectionStrings () =
-    [ for x in ConfigurationManager.ConnectionStrings -> x ]
-
-let OpenConnection () =
-    match ConnectionStrings () |> Seq.tryFind (fun x -> x.Name = connectionStringName) with
+let private OpenConnection () =
+    let connectionStringName = "Jnx"
+    let connectionStrings = [ for x in ConfigurationManager.ConnectionStrings -> x ]
+    match connectionStrings |> Seq.tryFind (fun x -> x.Name = connectionStringName) with
     | Some connectionString ->
         let connection = new NpgsqlConnection(connectionString.ConnectionString)
         connection.Open()
         connection
-    | _ -> failwith @"ConnectionString named ""Jnx"" was not found in configuration settings."
+    | _ -> failwith (sprintf @"ConnectionString named ""%s"" was not found in configuration settings." connectionStringName)
 
-let IsInitialized (connection : NpgsqlConnection) =
+let private IsInitialized (connection : NpgsqlConnection) =
     use command = connection.CreateCommand()
     command.CommandText <- @"select count(*) from information_schema.tables where table_catalog=:schemaname and table_name='migrations'"
     command.Parameters.AddWithValue("schemaname",  connection.Database) |> ignore
     unbox<int64> (command.ExecuteScalar()) > 0L
 
-let public InitDatabase () =
+let InitDatabase () =
     use connection = OpenConnection()
     if IsInitialized connection then
         failwith @"Schema is already initialized."
@@ -33,7 +28,7 @@ let public InitDatabase () =
     command.ExecuteNonQuery() |> ignore
     printfn "Ready!"
 
-let public RunMigrations () =
+let RunMigrations () =
     use connection = OpenConnection()
     if not (IsInitialized connection) then
         failwith @"Schema is not properly initialized. Run initialization (/I) first."
