211c3e1384688d5454858ceed8a9698ed371fc27
Janno Põldma <janno.poldma@gmail.com>
2014-06-05 14:39:49 +0300

    Upgraded packages. Refactored bootstrapper into separate source file.

---
+++ Jnx/Bootstrapper.fs
@@ -0,0 +1,31 @@
+﻿namespace Jnx
+
+open Jnx.Authentication
+open Jnx.NancyExtensions
+open Nancy
+open Nancy.Authentication.Forms
+open Nancy.Conventions
+open Nancy.Diagnostics
+open Nancy.Session
+
+type Bootstrapper() =
+    inherit DefaultNancyBootstrapper()
+
+    override this.ApplicationStartup (_, pipelines) =
+        CookieBasedSessions.Enable(pipelines) |> ignore
+        SessionFlashStore.Enable(pipelines)
+        DiagnosticsHook.Disable(pipelines)
+
+    override this.ConfigureConventions conventions =
+        base.ConfigureConventions conventions
+        conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Scripts", "Scripts", "js"))
+        conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("fonts", "fonts", "eot", "svg", "ttf", "woff"))
+
+    override this.ConfigureRequestContainer (container, context) =
+        base.ConfigureRequestContainer (container, context)
+        container.Register<IUserMapper, DatabaseUser>() |> ignore
+
+    override this.RequestStartup (container, pipelines, context) =
+        base.RequestStartup (container, pipelines, context)
+        let configuration = FormsAuthenticationConfiguration(RedirectUrl = "~/login", UserMapper = container.Resolve<IUserMapper>())
+        FormsAuthentication.Enable(pipelines, configuration)
--- Jnx/Program.fs
+++ Jnx/Program.fs
@@ -1,14 +1,7 @@
 module Jnx.Main
 
-open Jnx.NancyExtensions
-open Nancy
-open Nancy.Authentication.Forms
-open Nancy.Conventions
-open Nancy.Diagnostics
 open Nancy.Hosting.Self
-open Nancy.Session
 open System
-open System.Collections.Generic
 open System.Threading
 
 type RunningMode = Daemon | Application
@@ -39,28 +32,6 @@ let parseCommandLine args =
             eprintfn "Option '%s' is unrecognized" x
             parseCommandLineRec xs options
     parseCommandLineRec (args |> Seq.toList) defaultOptions
-
-type Bootstrapper() =
-    inherit DefaultNancyBootstrapper()
-
-    override this.ApplicationStartup (_, pipelines) =
-        CookieBasedSessions.Enable(pipelines) |> ignore
-        SessionFlashStore.Enable(pipelines)
-        DiagnosticsHook.Disable(pipelines)
-
-    override this.ConfigureConventions conventions =
-        base.ConfigureConventions conventions
-        conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("Scripts", "Scripts", "js"))
-        conventions.StaticContentsConventions.Add(StaticContentConventionBuilder.AddDirectory("fonts", "fonts", "eot", "svg", "ttf", "woff"))
-
-    override this.ConfigureRequestContainer (container, context) =
-        base.ConfigureRequestContainer (container, context)
-        container.Register<IUserMapper, Jnx.Authentication.DatabaseUser>() |> ignore
-
-    override this.RequestStartup (container, pipelines, context) =
-        base.RequestStartup (container, pipelines, context)
-        let configuration = FormsAuthenticationConfiguration(RedirectUrl = "~/login", UserMapper = container.Resolve<IUserMapper>())
-        FormsAuthentication.Enable(pipelines, configuration)
 
 [<EntryPoint>]
 let main args =
