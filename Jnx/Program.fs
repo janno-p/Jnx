3e257e42d8c9b868f66fa10c722a77a8ffe7f2df
Janno Põldma <janno.poldma@gmail.com>
2015-03-27 17:26:57 +0200

    Get Razor working with Suave.

--- Jnx/Program.fs
+++ Jnx/Program.fs
@@ -1,5 +1,51 @@
 ﻿open Suave
+open Suave.Http
+open Suave.Http.Applicatives
+open Suave.Http.Files
 open Suave.Http.Successful
+open Suave.Http.RequestErrors
+open Suave.Razor
+open Suave.Types
 open Suave.Web
+open System
+open System.Reflection
+open System.Security
+open System.Security.Permissions
 
-startWebServer defaultConfig (OK "Hello, World!")
+let mimeTypesMap = function
+    | ".css" -> Some { name = "text/css"; compression = true }
+    | ".js" -> Some { name = "application/x-javascript"; compression = true }
+    | _ -> None
+
+let app =
+    choose
+      [ GET >>= choose
+          [ path "/hello" >>= OK "Hello GET"
+            path "/goodbye" >>= OK "Good bye GET"
+            path "/home" >>= razor "Views/Site/Home.cshtml" None
+            path "/x" >>= (fun r -> async { return! OK (sprintf "%s" r.runtime.homeDirectory) r }) ]
+        GET >>= browseHome
+        POST >>= choose
+          [ path "/hello" >>= OK "Hello POST"
+            path "/goodbye" >>= OK "Good bye POST" ]
+        NOT_FOUND "Found no handlers" ]
+
+[<EntryPoint>]
+let main argv =
+    if AppDomain.CurrentDomain.IsDefaultAppDomain() then
+        printfn "Switching to second AppDomain, for RazorEngine..."
+        let setup = AppDomainSetup()
+        setup.ApplicationBase <- AppDomain.CurrentDomain.SetupInformation.ApplicationBase
+        let domain = AppDomain.CreateDomain(
+                        "MyMainDomain",
+                        null,
+                        AppDomain.CurrentDomain.SetupInformation,
+                        PermissionSet(PermissionState.Unrestricted),
+                        [| |])
+        let exitCode = domain.ExecuteAssembly(Assembly.GetExecutingAssembly().Location)
+        AppDomain.Unload(domain)
+        exitCode
+    else
+        printfn "%A" defaultConfig.homeFolder
+        app |> startWebServer { defaultConfig with mimeTypesMap = mimeTypesMap }
+        0
---
+++ Jnx/Suave.Razor.fs
@@ -0,0 +1,57 @@
+﻿namespace Suave
+
+open System
+open System.IO
+open Suave.Types
+open Suave.Types.Codes
+open Suave.Http
+open Suave.Http.Files
+open Suave.Utils
+
+open RazorEngine
+open RazorEngine.Configuration
+open RazorEngine.Templating
+
+module Razor =
+
+    let private memoize f =
+        let cache = Collections.Concurrent.ConcurrentDictionary<_,_>()
+        fun x ->
+            match cache.TryGetValue(x) with
+            | true, res -> res
+            | _ ->
+                let res = f x
+                cache.[x] <- res
+                res
+
+    let private loadTemplate path =
+        use file = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.ReadWrite)
+        use reader = new StreamReader(file)
+        reader.ReadToEnd()
+
+    let loadTemplateCached = memoize loadTemplate
+
+    type private MyTemplateManager() =
+        interface ITemplateManager with
+            override __.Resolve(key) =
+                upcast LoadedTemplateSource(loadTemplateCached key.Name)
+            override __.GetKey(name, resolveType, context) =
+                upcast NameOnlyTemplateKey(name, resolveType, context)
+            override __.AddDynamic(key, source) =
+                ()
+
+    let razor<'a> path (model : 'a) =
+        fun r -> async {
+            try
+                let s = loadTemplateCached path
+                let content = Engine.Razor.RunCompile(s, path, null, model)
+                return! Response.response HTTP_200 (UTF8.bytes content) r
+            with 
+                ex ->
+                return! Response.response HTTP_500 (UTF8.bytes (ex.ToString())) r
+        }
+
+    Engine.Razor <- RazorEngineService.Create(
+                        TemplateServiceConfiguration(
+                            Debug = true,
+                            TemplateManager = MyTemplateManager()))
